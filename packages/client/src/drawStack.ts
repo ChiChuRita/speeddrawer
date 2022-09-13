7bc153c4cf1b7703eba4e4865722f834c8e26bfd
chichurita <ra.singh069@gmail.com>
2022-09-13 11:05:03 +0200

    some futher implementations

--- packages/client/src/drawStack.ts
+++ packages/client/src/drawStack.ts
@@ -1,3 +1,27 @@
-class DrawStack {}
+import { Point } from "./drawer";
+import Color from "./color";
+
+export enum DrawType {
+    Begin,
+    End,
+    Pixel,
+    Line,
+    Rect,
+    Circle,
+    Clear,
+    Undo,
+    Redo,
+}
+
+interface DrawCommand {
+    type: DrawType;
+    points: Point[];
+    color: Color;
+}
+
+class DrawStack {
+    private undoStack: Array<DrawCommand> = [];
+    private redoStack: Array<DrawCommand> = [];
+}
 
 export default DrawStack;
--- packages/client/src/drawer.ts
+++ packages/client/src/drawer.ts
@@ -1,3 +1,11 @@
+import bresenham from "bresenham";
+import Color from "./color";
+
+export interface Point {
+    x: number;
+    y: number;
+}
+
 class Drawer {
     private readonly ctx: CanvasRenderingContext2D;
 
@@ -5,15 +13,37 @@ class Drawer {
         this.ctx = ctx;
     }
 
-    public drawRect(
-        x: number,
-        y: number,
-        width: number,
-        height: number,
-        color: string
-    ) {
-        this.ctx.fillStyle = color;
-        this.ctx.fillRect(x, y, width, height);
+    public drawPixel(point: Point, color: Color) {
+        this.ctx.fillStyle = color.toString();
+        this.ctx.fillRect(point.x, point.y, 1, 1);
+    }
+
+    public drawLine(firstPoint: Point, secondPoint: Point, color: Color) {
+        const points = bresenham(
+            firstPoint.x,
+            firstPoint.y,
+            secondPoint.x,
+            secondPoint.y
+        );
+        points.forEach((point) => this.drawPixel(point, color));
+    }
+
+    public drawRect(firstPoint: Point, secondPoint: Point, color: Color) {
+        this.ctx.fillStyle = color.toRGBAString();
+        this.ctx.fillRect(
+            firstPoint.x,
+            firstPoint.y,
+            secondPoint.x - firstPoint.x,
+            secondPoint.y - firstPoint.y
+        );
+    }
+
+    public drawCircle() {
+        //TODO
+    }
+
+    public clear() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
 }
 
